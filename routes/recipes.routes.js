2ad9bc4eb26c16a1c2238c773947c633f55833e7
Trey_cie <kusiwaahboateng1997@gmail.com>
2024-03-27 04:06:35 -0700

    getting recipe image upload to work

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,19 +1,23 @@
 import { Router } from "express";
-import { addRecipe, getRecipe, getRecipes,updateRecipe,deleteRecipe } from "../controllers/recipes.controller.js";
+import { addRecipe, getRecipe, getRecipes, updateRecipe, deleteRecipe } from "../controllers/recipes.controller.js";
+import multer from "multer";
 
+// Create multer upload middleware
+const upload = multer({ dest: 'uploads/images' });
 
+// Create recipes router
 const router = Router();
 
 
 // Define your routes
- 
-router.post('/recipes', addRecipe);
+
+router.post('/recipes', upload.single('image'), addRecipe);
 
 router.get('/recipes', getRecipes);
 
 router.get('/recipes/:id', getRecipe);
 
-router.patch('/recipes/:id',updateRecipe);
+router.patch('/recipes/:id', updateRecipe);
 
 router.delete('/recipes/:id', deleteRecipe);
 
